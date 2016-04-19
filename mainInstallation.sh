a6345d1055dcbbed58c6ac6346aba9d5698ff79e
jasmine <infoguidesgirl@gmail.com>
2016-04-19 23:20:22 +1000

    Add comments

--- mainInstallation.sh
+++ mainInstallation.sh
@@ -24,7 +24,7 @@ echo -n "The current directory before creating mocha.opts: "
 pwd
 sleep 1
 echo -e "--require should \n--ui bdd \n--recursive" > mocha.opts
-cd /home/ubumtdd/$projectName
+cd ~$projectName
 sed -i -e '$ i , "devDependencies": { \n  "mocha": "*", \n  "should": ">= 0.0.1" \n } ' package.json
 sed -i -e '/scripts/ a \    "test": "node_modules/.bin/mocha -w",' package.json
 npm install
@@ -32,6 +32,11 @@ cd test
 echo -e "'use strict'; \n \nvar myCode = require('./code.js'); //The string on this line is the location of the file to be tes$
 echo -e "'use strict';  \nvar number = 1; \nexports.outputNumber = number; \n" > code.js
 npm install
+echo -n "You are now set up to do Test Driven Development with NodeJS. Your project directory is located at ~/"$projectName"."
+echo "Inside your project directory, you will see a spec.js file for specifying the output requirements of your code. \nThere is also a code.js file which is where you can place the code which you want to test. \nThese two files already have some sample code in them. \nType navigate to your project directory and type  npm test  to see whether the code passes the output requirements of the spec.js file".
+
+
+
 
 
 
