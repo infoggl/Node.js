2774f305cd4333ec8105df0d9f93405dc5a636ff
infoggl <infoguidesgirl@gmail.com>
2016-04-23 02:09:20 +1000

    fine-tune outputs

--- mainInstallation.sh
+++ mainInstallation.sh
@@ -6,12 +6,13 @@ tar xvf node-v0.10.32-linux-x64.tar.gz
 cd node-v0.10.32-linux-x64/
 cp * /usr/local/ -r
 cd ~
-echo -n "You now have the following version of Node installed: "
+clear
+printf "\n You now have the following version of Node installed: "
 node -v
-sleep 1
-echo""
+sleep 0.1
+printf "\n"
 npm install -g express-generator
-echo -n "Please enter a name for your new project and press [Enter]: "
+printf "\n Please enter a name for your new project and press [Enter]: "
 read projectName
 express $projectName
 cd $projectName
@@ -26,14 +27,13 @@ sed -i -e '$ i , "devDependencies": { \n  "mocha": "*", \n  "should": ">= 0.0.1"
 sed -i -e '/scripts/ a \    "test": "node_modules/.bin/mocha -w",' package.json
 npm install
 cd test
-echo -e "'use strict'; \n \nvar myCode = require('./code.js'); //The string on this line is the location of the file to be tes$
-echo -e "'use strict';  \nvar number = 1; \nexports.outputNumber = number; \n" > code.js
+printf "'use strict'; \n \nvar myCode = require('./code.js'); //The string on this line is the location of the file to be tested\n \ndescribe('spec1', function () { \n  it('Code file should output the number 1', function (done) {  //The string on this line is just your own description of the test\n \n    var codeOut = myCode.outputNumber; \n    codeOut.should.equal(1); \n    done();\n\n  });\n});\n" > spec.js
+printf "'use strict';  \nvar number = 1; \nexports.outputNumber = number; \n" > code.js
 npm install
-echo -n "You are now set up to do Test Driven Development with NodeJS. Your project directory is located at ~/"$projectName"."
-echo -e "Inside your project directory, you will see a spec.js file for specifying the output requirements of your code. \nThere is also a code.js file which is where you can place the code which you want to test. \nThese two files already have some sample code in them. \nNnavigate to your project directory and type  npm test  to see whether the code passes the output requirements of the spec.js file".
-
-
-
-
-
-
+clear
+printf '\nCongratulations!\n\nYou are now set up to do Test Driven Development with NodeJS. Your project directory is located at ~/'$projectName'.'
+sleep 0.5
+printf '\nInside your project directory, you will see a ''\e[0;32m%-6s\e[m' "spec.js "
+printf 'file for specifying the output requirements of your code. \n\nThere is also a ''\e[0;32m%-6s\e[m' "code.js "
+printf 'file which is where you can place the code which you want to test. \nThese two files already have some sample code in them to get you started. \n\nNavigate to your project directory and type''\e[0;32m%-6s\e[m' " npm test "
+printf 'to see whether the code passes the output requirements of the spec.js file. \n\n'
